acae6f870dd3ac91861c8cf7cb780a2e69f2cc25
Nikita <natetkovnik@icloud.com>
2022-05-23 22:13:59 +0300

    Second lesson

--- project 1/script.js
+++ project 1/script.js
@@ -1,1 +1,15 @@
- const NumbersOfFilms = prompt('Сколько фильмов вы уже посмотрели?')
+ const NumbersOfFilms = +prompt('Сколько фильмов вы уже посмотрели?',"");
+ const PersonalMovieDB = {
+     count:NumbersOfFilms,
+     movies:{},
+     actors:{},
+     genres:[],
+     privat:false
+ };
+ const a = prompt("Один из последних");
+ const b = prompt("Оцени его");
+ const d = prompt("Один из последних");
+ const e = prompt("Оцени его");
+ PersonalMovieDB.movies[a] = b;
+ PersonalMovieDB.movies[d] = e;
+ console.log(PersonalMovieDB);
